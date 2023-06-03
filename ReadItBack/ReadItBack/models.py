4d52b8a349a11aa2f4fb3eeb1b42909ff49fa961
Benson Lee <BensonL1334@gmail.com>
2023-06-02 23:44:39 -0400

    Re-committing models created

---
+++ ReadItBack/ReadItBack/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class GutenbergText(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+    page = models.IntegerField()
+
+
+class User(AbstractUser):
+    library = models.ManyToManyField(GutenbergText, related_name='users')
