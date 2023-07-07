8cebffb0227c4dc889216c7b6b0ce6a93957595c
Benson Lee <BensonL1334@gmail.com>
2023-07-06 23:44:22 -0400

    Back to the beginning

--- App/models.py
+++ App/models.py
@@ -1,27 +1,2 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-
-class GutenbergText(models.Model):
-    name = models.CharField(max_length=200, unique=True)
-    author = models.CharField(max_length=100)
-    genre = models.CharField(max_length=50)
-    page = models.IntegerField()
-
-    class Meta:
-        using = 'gutenberg'
-
-
-class Category(models.Model):
-    genre = models.CharField(max_length=50)
-    text = models.ManyToManyField(GutenbergText, related_name='categories')
-
-    class Meta:
-        using = 'default'
-
-
-class User(AbstractUser):
-    library = models.ManyToManyField(GutenbergText, related_name='users')
-
-    class Meta:
-        using = 'default'
